b600a1dfc0accfaf516b19d150d3e1d83d90f48d
marija2102ua <marija2102ua@gmail.com>
2020-12-15 20:23:42 +0200

    added some minor changes

--- HW3/hw3.2.py
+++ HW3/hw3.2.py
@@ -1,18 +1,9 @@
-value = int(input("Введыть чотирицифрове натуральне число "))
-fourth = value % 10
-third = value // 10 % 10
-second = value // 100 % 10
-first = value // 1000
-multiply = first*second*third*fourt
-print(first*second*third*fourth)
+value = input("Введыть чотирицифрове натуральне число ")
+multiply = int(value[0])*int(value[1])*int(value[2])*int(value[3])
+reverse = value[::-1]
+sort = ''.join(sorted(value))
 
+print(f"multiply: {multiply}")
+print(f"reversed: {reverse}")
+print(f"sort: {sort}")
 
-
-# value = input("Введыть чотирицифрове натуральне число ")
-# multiply = int(value[0])*int(value[1])*int(value[2])*int(value[3])
-# reverse = value[::-1]
-# sort = ''.join(sorted(value))
-#
-# print(multiply)
-# print(reverse)
-# print(sort)
