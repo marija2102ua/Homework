b8b0465573e977bb144a59167197b3c2cf95cdad
marija2102ua <marija2102ua@gmail.com>
2020-12-16 14:16:37 +0200

    added hw6.1

---
+++ HW6/hw6.1.py
@@ -0,0 +1,10 @@
+def compare(a, b):
+    """This function returns the largest number of two numbers"""
+    if a > b:
+        return a
+    return value_2
+
+
+value_1 = 45
+value_2 = 20
+print(compare(value_1, value_2))
