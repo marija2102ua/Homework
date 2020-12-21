6079b576d9f78013f6ce52fb22b94be1be62002a
marija2102ua <marija2102ua@gmail.com>
2020-12-21 13:18:41 +0200

    added HW7 tasks 1 and modules

---
+++ HW7/hw7.1.py
@@ -0,0 +1,29 @@
+from modules.square_math import rectangle,triangle,circle
+
+
+def main():
+    while True:
+        num = input("""What do you want to calculate?
+1.calculates the square of a rectangle
+2.calculates the square of triangle
+3.calculates the square of circle
+Your choice: """)
+        if num == "1":
+            sideA = float(input("Enter A: "))
+            sideB = float(input("Enter B: "))
+            print(f'Square: {rectangle(sideA, sideB)}')
+        elif num == "2":
+            sideA = float(input("Enter A: "))
+            sideB = float(input("Enter B: "))
+            sideC = float(input("Enter C: "))
+            print(f'Square: {triangle(sideA, sideB, sideC)}')
+
+        elif num == "3":
+            radius = float(input("Enter radius: "))
+            print(f'Square: {circle(radius)}')
+        else:
+            print("Please make a choice")
+
+
+if __name__ == '__main__':
+    main()
---
+++ HW7/modules/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ["square_math"]
+
